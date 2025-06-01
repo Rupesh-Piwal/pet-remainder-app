f836def90bdd0232973252a427073a6b0d7efa04
rupesh <rpiwal02@gmail.com>
2025-06-01 15:39:53 +0530

    project intialization

---
+++ next.config.ts
@@ -0,0 +1,16 @@
+// next.config.ts
+import type { NextConfig } from "next";
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+};
+
+export default withPWA(nextConfig);
